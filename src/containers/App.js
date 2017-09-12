bb19790247d90f66d5223460bb1c6a0faaadc4a8
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-12 16:20:08 -0700

    trying to get webpack bundling

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        This is a test, this is only a test
+      </div>
+    )
+  }
+}
