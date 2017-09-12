234e8ec86b29c8917f89ba39299c35a55b63eb48
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-12 16:21:51 -0700

    added newGame constructor and key handling

--- public/main.js
+++ public/main.js
@@ -9,3 +9,32 @@ const store = {
   bodyPartId: 0,
   topScore: 0,
 }
+
+const newGame = () => {
+  const apple = new Apple();
+  const head = new Head();
+};
+
+const board = document.querySelector('#board');
+const container = document.querySelector('#container');
+
+const initializeKeys = () => {
+  window.addEventListener('keydown', function(e) {
+    if (e.keyCode === 40 && store.currentDirection !== 'up') {
+      e.preventDefault();
+      store.currentDirection = 'down';
+    } else if (e.keyCode === 39 && store.currentDirection !== 'left') {
+      e.preventDefault();
+      store.currentDirection = 'right';
+    } else if (e.keyCode === 38 && store.currentDirection !== 'down') {
+      e.preventDefault();
+      store.currentDirection = 'up';
+    } else if (e.keyCode === 37 && store.currentDirection !== 'right') {
+      e.preventDefault();
+      store.currentDirection = 'left';
+    } else if (e.keyCode === 32 && store.gameOver) {
+      e.preventDefault();
+      resetBoard();
+    }
+  });
+};
