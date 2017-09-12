4c116ca561a0fe61e9097eb62474573a3560fea0
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-12 15:47:48 -0700

    create store object

---
+++ public/main.js
@@ -0,0 +1,11 @@
+const store = {
+  apple: { top: 0, left: 0 },
+  head: { top: 0, left: 0 },
+  speed: 300,
+  currentDirection: 'down',
+  gameOver: false,
+  score: 0,
+  body: [],
+  bodyPartId: 0,
+  topScore: 0,
+}
