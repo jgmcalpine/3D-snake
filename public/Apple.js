faeeae265751b572e6133e5d87a38169443f3d7d
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-13 18:21:26 -0700

    fixed apple appearing on body issue

--- public/Apple.js
+++ public/Apple.js
@@ -3,19 +3,25 @@ class Apple {
     this.node = document.createElement('div');
     this.node.setAttribute('id', 'apple');
     board.appendChild(this.node);
-
+    console.log('store length', store.body.length);
     setApple();
   }
 }
 
 function setApple() {
   const appleNode = document.getElementById('apple');
-  const leftLocation = Math.floor(Math.random() * 12) * 50;
-  const topLocation = Math.floor(Math.random() * 12) * 50;
+  let leftLocation;
+  let topLocation;
+  function findRandomLocation() {
+    leftLocation = Math.floor(Math.random() * 12) * 50;
+    topLocation = Math.floor(Math.random() * 12) * 50;
+  }
+  
 
   for (let i = 0; i < store.body.length; i += 1) {
     if (leftLocation === store.body[i].left && topLocation === store.body[i].top) {
-      setApple();
+      findRandomLocation();
+      i--;
     }
   }
 
