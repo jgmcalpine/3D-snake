ce4a9b0c85bdd1ee57f8da3d95379be5d79dc27b
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-13 17:42:19 -0700

    added basic styling

--- public/Body.js
+++ public/Body.js
@@ -15,3 +15,27 @@ class Body {
     store.bodyPartId += 1;
   }
 }
+
+function growSnake() {
+  const bodySection = new Body();
+}
+
+// Move snake
+function moveSnake() {
+  for (let i = 0; i < store.body.length - 1; i += 1) {
+    store.body[i].top = store.body[i + 1].top;
+    store.body[i].left = store.body[i + 1].left;
+  }
+
+  if (store.body.length > 0) {
+    let snakeNeck = store.body[store.body.length - 1];
+    snakeNeck.top = store.head.top;
+    snakeNeck.left = store.head.left;
+  }
+
+  for (let i = 0; i < store.body.length - 1; i += 1) {
+    let currNode = document.getElementById('snake-' + i);
+    currNode.style.transition = `transform ${store.speed}ms linear`;
+    currNode.style.transform = `translate(${store.body[i].left}px, ${store.body[i].top}px) translateZ(50px)`;
+  }
+}
