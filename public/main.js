a9a47f784fcbef45e248863f49c23b536ff6a5fc
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-13 17:44:23 -0700

    added changes to score and speed and check edges and cannibal

--- public/main.js
+++ public/main.js
@@ -38,3 +38,37 @@ const initializeKeys = () => {
     }
   });
 };
+
+function incrementScore() {
+  if (store.speed > 150) {
+    store.score += 20;
+    document.querySelector('#score span').innerHTML = store.score;
+  } else {
+    store.score += 50;
+    document.querySelector('#score span').innerHTML = store.score;
+  }
+}
+
+function increaseSpeed() {
+  if (store.speed > 200) store.speed -= 20;
+  else if (store.speed > 100) store.speed -= 10;
+  else store.speed -=5;
+}
+
+function checkEdges() {
+  if (store.head.top > 550 || store.head.top < 0 || store.head.left < 0 || store.head.left > 550) {
+    setTopScore();
+    store.gameOver = true;
+    playAgainMessage();
+  }
+}
+
+function checkCannibal() {
+  for (let i = 0; i < store.body.length; i += 1) {
+    if (store.head.top === store.body[i].top && store.head.left === store.body[i].left) {
+      setTopScore();
+      playAgainMessage();
+      store.gameOver = true;
+    }
+  }
+}
